43d38d2759dfb0a06fc2463fa2dc80585f6bcbb6
PhuocLe04 <lehuuphuoc0804z@gmail.com>
2024-11-02 23:16:35 +0700

    SPRINT2_QUAN LY DON HANG VA ADMIN NHAP HANG

--- TapHoa/obj/Debug/net8.0/TapHoa.AssemblyInfo.cs
+++ TapHoa/obj/Debug/net8.0/TapHoa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TapHoa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1bcd8584f73529fde1b47485d498d88717e9d75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+713d3140c74db1eea258a78652301fd1c47cd2ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("TapHoa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TapHoa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
